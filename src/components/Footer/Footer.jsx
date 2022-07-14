bedd544e78de26fec67ac2151d628bb87f2317d9
Chibuzor <chibuzorm26@example.com>
2022-07-14 20:03:18 +0100

    final commit.. for now

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
-import './Footer.css';
+import './Footer.css'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+       <div className='footer__copyright'>
+        <small>&copy; BUZOR. All rights reserved.</small>
+       </div>
+    </footer>
   )
 }
 
-export default Footer
+export default Footer 
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,16 +1,29 @@
-import React from 'react'
-import './Header.css';
+import React from "react";
+import "./Header.css";
+import { IoMdPhotos } from "react-icons/io";
 
 const Header = () => {
   return (
     <header>
       <div className="container header__container">
         <h5>Welcome to</h5>
-        <h1><span class="sign__word">The_Chibuzor's Art.</span></h1>
+        <h1>
+          <span class="sign__word">The_Chibuzor's Art.</span>
+        </h1>
         <h5 className="text-light">I'm going to blow your mind.</h5>
+        <h2>
+          Updated album available here:{" "}
+          <a
+            href="https://photos.google.com/share/AF1QipPySAB1mGBpIB970-JjSav4zrvhxes93CnBVDeDEcJCEdW5cBEq7X-yGv-OYGoOaw?key=aWhWcmxQcm0zQnRkTWtQTnFRTjJ1VmVnN1RuODNn"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <IoMdPhotos className="album_icon"/>
+          </a>
+        </h2>
       </div>
     </header>
-  )
-}
+  );
+};
 
 export default Header;
