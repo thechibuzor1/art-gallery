baae9b9c876d9ffb9a0f91fffc675dc03cd286c8
Chibuzor <chibuzorm26@example.com>
2022-07-17 13:58:40 +0100

    cl fix

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Footer from "./components/Footer/Footer";
 import Characters from "./screens/characters";
 import Creepy from "./screens/creepy";
